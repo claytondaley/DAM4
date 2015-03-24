16ff757fdef7221fc547ae8196daf0a9cb4ec1f6
Clayton Daley <clayton.daley@gmail.com>
2015-03-23 22:53:25 -0400

    - enable cache  - disable ZendDeveloperTools

--- config/application.config.php
+++ config/application.config.php
@@ -3,7 +3,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     // Prioritization is LIFO
     'modules' => array(
-        'ZendDeveloperTools',
+        # 'ZendDeveloperTools',
         'DoctrineModule',
         'DoctrineORMModule',
         # LegacyRS includes a wildcard router so no page-delivering modules may be placed after it
@@ -48,21 +48,21 @@ return array(
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        //'config_cache_enabled' => $booleanValue,
+        'config_cache_enabled' => true,
 
         // The key used to create the configuration cache file name.
-        //'config_cache_key' => $stringKey,
+        'config_cache_key' => "dam4",
 
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        //'module_map_cache_enabled' => $booleanValue,
+        'module_map_cache_enabled' => true,
 
         // The key used to create the class map cache file name.
-        //'module_map_cache_key' => $stringKey,
+        // 'module_map_cache_key' => $stringKey,
 
         // The path in which to cache merged configuration.
-        //'cache_dir' => $stringPath,
+        'cache_dir' => "./data/cache",
 
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
